de092f1a4dbe1b89dc167b27459945604d10daad
Richard Price <home@richardprice.me>
2018-03-06 10:18:15 +0000

    adds in missing exceptions file

---
+++ exceptions.py
@@ -0,0 +1,9 @@
+try:
+    from django.contrib.auth.models import SiteProfileNotAvailable
+except ImportError:
+    class SiteProfileNotAvailable(Exception):
+        pass
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                                                                                                                                                                                                  
+~                                
